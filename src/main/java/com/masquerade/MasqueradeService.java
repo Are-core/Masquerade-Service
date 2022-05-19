1f0d104b4404239f6a1b88baa43e4c527899a10b
Arekor <bunner.benjamin@gmail.com>
2022-05-19 19:48:53 +0200

    Settings for WebService maven

--- src/main/java/com/masquerade/MasqueradeService.java
+++ src/main/java/com/masquerade/MasqueradeService.java
@@ -1,4 +1,13 @@
 package com.masquerade;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService(serviceName = "MasqueradeService")
 public class MasqueradeService {
+
+    @WebMethod
+    public String archetypeList(int value) {
+        return "List here : " + value;
+    }
 }
