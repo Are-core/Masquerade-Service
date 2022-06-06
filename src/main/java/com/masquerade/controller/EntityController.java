9c0fbdb96035074ad02f0e1118722d290bfebcab
Arekor <bunner.benjamin@gmail.com>
2022-06-06 14:12:18 +0200

    Modify - Change get character method by id

--- src/main/java/com/masquerade/controller/EntityController.java
+++ src/main/java/com/masquerade/controller/EntityController.java
@@ -6,10 +6,7 @@ import com.masquerade.model.SmallEntity;
 import com.masquerade.service.EntityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,8 +24,8 @@ public class EntityController {
         return new ResponseEntity<>(entityService.getList(), HttpStatus.OK);
     }
 
-    @RequestMapping(value ="/getEntity",method = RequestMethod.GET)
-    public ResponseEntity<EntityEntity> getEntity(Long id) throws BadRequestException {
+    @RequestMapping(method = RequestMethod.GET, path = "character/{id}")
+    public ResponseEntity<EntityEntity> get(@PathVariable Long id) throws BadRequestException {
         return new ResponseEntity<>(entityService.getEntity(id), HttpStatus.OK);
     }
 }
