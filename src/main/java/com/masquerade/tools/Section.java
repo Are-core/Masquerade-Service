5ed404df259231bda4b7a7c6c6b4aed9f623fdac
Arekor <bunner.benjamin@gmail.com>
2022-06-25 11:00:44 +0200

    Modify - Rework service path prefixes

--- src/main/java/com/masquerade/tools/Section.java
+++ src/main/java/com/masquerade/tools/Section.java
@@ -11,11 +11,15 @@ public class Section {
     public static final String CharacterPrefix = "";
 
     /* CHARACTER PARAMETERS */
-        public static final String StatusPrefix = CharacterSheetParameterPrefix;
-        public static final String ArchetypePrefix = CharacterSheetParameterPrefix;
-        public static final String ClanPrefix = CharacterSheetParameterPrefix;
-        public static final String JurisdictionPrefix = CharacterSheetParameterPrefix;
-        public static final String SectPrefix = CharacterSheetParameterPrefix;
-        public static final String StatusTypePrefix = CharacterSheetParameterPrefix;
-        public static final String TitlePrefix = CharacterSheetParameterPrefix;
+
+        /* GLOBAL */
+        public static final String ArchetypePrefix = CharacterSheetParameterPrefix + "/archetype";
+        public static final String ClanPrefix = CharacterSheetParameterPrefix + "/clan";
+        public static final String SectPrefix = CharacterSheetParameterPrefix + "/sect";
+        public static final String TitlePrefix = CharacterSheetParameterPrefix + "/title";
+
+        /* PARAMETER */
+        public static final String JurisdictionPrefix = CharacterSheetParameterPrefix + "/jurisdiction";
+        public static final String StatusPrefix = CharacterSheetParameterPrefix + "/status";
+        public static final String StatusTypePrefix = CharacterSheetParameterPrefix + "/status/type";
 }
