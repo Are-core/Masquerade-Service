c61ea14e2bd5294f776233019948804aa62ce499
Arekor <bunner.benjamin@gmail.com>
2022-07-02 12:32:43 +0200

    Feature - Application : Add JUnit test

--- src/main/java/com/masquerade/MasqueradeApplication.java
+++ src/main/java/com/masquerade/MasqueradeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MasqueradeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MasqueradeApplication.class, args);
+        SpringApplication.run(MasqueradeApplication.class, args
+        );
     }
 }
